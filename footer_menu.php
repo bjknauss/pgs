105f1461e11fad76e270239c14a008d84b6e597c
Brenden Knauss <bjknauss@live.com>
2017-03-24 21:31:56 -0700

    Fixed a few links which redirected to .html instead of .php

--- footer_menu.php
+++ footer_menu.php
@@ -1,11 +1,11 @@
     <footer class="row">
         <div class="col-xs-1 col-md-2"></div>
         <div class="hidden-xs hidden-sm col-md-2"><small>Peter's Garden Service</small></div>
-        <div class="col-xs-2 col-md-1"><a href="index.html"><small>Home</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="services.html"><small>Services</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="gallery.html"><small>Gallery</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="about.html"><small>About</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="contact.html"><small>Contact</small></a></div>
+        <div class="col-xs-2 col-md-1"><a href="index.php"><small>Home</small></a></div>
+        <div class="col-xs-2 col-md-1"><a href="services.php"><small>Services</small></a></div>
+        <div class="col-xs-2 col-md-1"><a href="gallery.php"><small>Gallery</small></a></div>
+        <div class="col-xs-2 col-md-1"><a href="about.php"><small>About</small></a></div>
+        <div class="col-xs-2 col-md-1"><a href="contact.php"><small>Contact</small></a></div>
         <div class="col-xs-1 col-md-3"></div>
         </div>
     </footer>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <h2>Welcome to Peter's Garden Service</h2>
             <p>With 20 years of experience, we can cater to all your residential or commercial landscaping needs. Our services include weeding, trimming, pruning, edging, and more.</p>
 
-            <p><a href="services.html" class="btn btn-primary btn-lg">See Full List of Services</a></p>
+            <p><a href="services.php" class="btn btn-primary btn-lg">See Full List of Services</a></p>
         </div>
         <div class="col-sm-4 hidden-xs">
             <img class="img-responsive img-rounded" src="images/index.jpg" alt="Landscaped Yard with Retaining Wall">
@@ -22,17 +22,7 @@
     </div><!-- end row 3 -->
     
     <!-- row 4 -->
-    <footer class="row">
-        <div class="col-xs-1 col-md-2"></div>
-        <div class="hidden-xs hidden-sm col-md-2"><small>Peter's Garden Service</small></div>
-        <div class="col-xs-2 col-md-1"><a href="index.html"><small>Home</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="services.html"><small>Services</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="gallery.html"><small>Gallery</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="about.html"><small>About</small></a></div>
-        <div class="col-xs-2 col-md-1"><a href="contact.html"><small>Contact</small></a></div>
-        <div class="col-xs-1 col-md-3"></div>
-        </div>
-    </footer>
+    <?php include_once("footer_menu.php"); ?>
 
 </div> <!-- end container -->
 <script>
